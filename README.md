49811e9eb83fb89fa3bf537cd6b68c2e88dd0824
Deryk <jamaboderek@outlook.com>
2017-06-13 22:51:03 +0300

    modified:   README.md 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.module.ts 	.angular-cli.json 	src/app/app.component.css 	src/app/app.component.html 	src/app/error-pages/error-pages.component.css 	src/app/home/home.component.css 	src/app/live-exchange/live-exchange.component.css 	src/app/register/register.component.css 	src/app/reset-password/reset-password.component.css 	src/assets/.gitkeep 	src/assets/css/style.css 	src/assets/img/Mylogo/ 	src/assets/img/breadcrumb.png 	src/assets/img/demo/ 	src/assets/img/gallery/imgbox3.jpg 	src/assets/img/hue.png 	src/assets/img/icons/ 	src/assets/img/logo.png 	src/assets/img/select2.png

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # StockExchange
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.0.1.
-## Installation 
-cd to StockExchange folder and Run `npm install`  to install dependencies.  
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { LiveExchangeComponent } from './live-exchange/live-exchange.component';
 
 
 const routes : Routes = [
+  {path: '' , component: LoginComponent},
   {path: 'login', component: LoginComponent},
   {path: 'dashboard', component: HomeComponent},
   {path: 'live-exchange', component: LiveExchangeComponent},
