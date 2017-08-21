b55fc716d3f5bfc286c042165ef5fae3e835a36c
Deryk <jamaboderek@outlook.com>
2017-08-22 00:25:30 +0300

    Check src/app/protected/protected-routing.module.ts for routing example.

--- src/app/protected/protected-routing.module.ts
+++ src/app/protected/protected-routing.module.ts
@@ -2,21 +2,44 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ErrorPagesComponent } from '../error-pages/error-pages.component';
+import { BankInfoComponent } from './bank-info/bank-info.component';
+import { BankTransferComponent } from './bank-transfer/bank-transfer.component';
 import { HomeComponent } from './home/home.component';
 import { SideMenuComponent } from './side-menu/side-menu.component';
+import { LiveExchangeComponent } from './live-exchange/live-exchange.component';
 
+
+/**
+ * This is where u import all the protected folders for navigation/
+ * path is the name of path you'll use in routerLink.
+ *
+ * Note: Dont forget to import the associated component..
+ *        any component import here must be added to the declaration object in protected.module.
+ * */
 const protected_routes: Routes = [
   {
     path     : 'dashboard',
     component: SideMenuComponent,
     children : [
       {
-        path     : 'home',
+        path     : 'home',  // name of path
+        component: HomeComponent // component for home.
+      },
+      {
+        path     : '',  // empty path routes to home as well
         component: HomeComponent
       },
       {
-        path     : '',
-        component: HomeComponent
+        path     : 'bank-info',
+        component: BankInfoComponent
+      },
+      {
+        path     : 'live-exchange',
+        component: LiveExchangeComponent
+      },
+      {
+        path     : 'bank-transfer',  // can use this now like so routerLink="bank-transfer" in side-menu.component.html
+        component: BankTransferComponent,
       },
       {
         path     : '**',
